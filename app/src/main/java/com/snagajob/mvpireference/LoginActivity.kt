0d37cf62bcc30ca6c668d55c3a16ef9cf4a86e4f
Keith Kienzle <keith.kienzle@snagajob.com>
2017-11-30 14:20:02 -0500

    Add crashlytics and google services.

--- app/src/main/java/com/snagajob/mvpireference/LoginActivity.kt
+++ app/src/main/java/com/snagajob/mvpireference/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.snagajob.mvpireference
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_login)
     }
 }
