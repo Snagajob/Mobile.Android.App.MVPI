cb217a902817d107f4d9d82718106a73bc63f8e4
CoreyHorn <corey.horn@snagajob.com>
2018-02-15 11:25:47 -0500

    Update Framework and Network Models Update LoginService to reflect current pattern Improve Documentation

--- app/src/main/java/com/snagajob/mvpireference/services/login/LoginResponse.kt
+++ app/src/main/java/com/snagajob/mvpireference/services/login/LoginResponse.kt
@@ -1,6 +1,18 @@
 package com.snagajob.mvpireference.services.login
 
-data class LoginResponse(val memberId: String, val tokens: List<CustomerToken>)
+data class LoginResponse(val member: MemberDetail,
+                         val tokens: List<CustomerToken>)
 
-data class CustomerToken(val securityToken: String, val customerId: String,
-                         val customerName: String, val expirationDateTime: String)
+data class CustomerToken(val token: String,
+                         val customerId: String,
+                         val customerName: String,
+                         val expirationDateTime: String)
+
+data class MemberDetail(val id: String,
+                        val users: List<UserDetail>)
+
+data class UserDetail(val id: String,
+                      val customerId: String,
+                      val displayName: String,
+                      val avatarUrl: String?,
+                      val modules: List<String>)
